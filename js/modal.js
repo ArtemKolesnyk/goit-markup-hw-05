d1898fc6a7b37cb25eae8ee3b18821068a4a1fdc
ArtemKolesnyk <kolesnik2405@gmail.com>
2021-06-27 20:14:41 +0200

    fixed business

--- js/modal.js
+++
@@ -1,9 +0,0 @@
-const heroButtonRef = document.querySelector('.js-hero-button');
-const wrapperPopupRef = document.querySelector('.js-wrapper-popup');
-const closePopupRef = document.querySelector('.js-close-popup');
-
-heroButtonRef.addEventListener('clik', togglePopup);
-
-function togglePopup() {
-    wrapperPopupRef.classList.toggle('open');
-}
